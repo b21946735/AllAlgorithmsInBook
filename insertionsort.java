7f7b88424c00cae19a8c5dad298e7fb683d7f1a0
b21946735 <b21946735@cs.hacettepe.edu.tr>
2022-01-16 23:26:16 +0300

    InsertionSort

--- insertionsort.java
+++ insertionsort.java
@@ -2,11 +2,12 @@ public class insertionsort {
     public static int[] insertionSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             int currentElement = arr[i];
-            for (int j = i-1; j >=0; j--) {
+            for (int j = i-1; j >=0; j--) { // start from currentindex-1 to index 0 and compare with currentelement and shift currentelement
                 if (arr[j] > currentElement ) {
                     arr[j+1] = arr[j];
                     arr[j] = currentElement;
                 }
+                else{break;} // prevent unnecessary compares
             }
         }
 
@@ -17,7 +18,7 @@ public class insertionsort {
         int[] sorted = insertionSort(arr);
 
         for (int i : sorted) {
-            System.out.println(i);
+            System.out.print(i +" "); // output 1 1 2 3 3 4 5 7 9 16 
         }
     }
 }

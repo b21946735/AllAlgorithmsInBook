d72bd31f4d6cbfd13e1c45130bdc4879603eed34
b21946735 <b21946735@cs.hacettepe.edu.tr>
2022-01-31 22:46:17 +0300

    BubbleSort

--- bubblesort.java
+++ bubblesort.java
@@ -1,9 +1,9 @@
 public class bubblesort {
 
     public static void bubbleSort(int[] arr){
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 1; i < arr.length-1; i++) {
             boolean swapped = false;
-            for (int j = 0; j < arr.length-i-1; j++) {
+            for (int j = 0; j < arr.length-i; j++) {
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
